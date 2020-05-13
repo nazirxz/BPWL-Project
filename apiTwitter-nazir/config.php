c983d6f6bc0201af62aad73d7a2682da3f7a5f98
Nazirman <nzryanzr@gmail.com>
2020-05-13 09:13:16 +0700

    Modul 6

---
+++ apiTwitter-nazir/config.php
@@ -0,0 +1,8 @@
+<?php 
+
+	Define("CONSUMER_API_KEY","N4SWdyqK2pGlH6CC5jnNLrL0w");
+	Define("CONSUMER_API_SECRET","n78c3GeFLsGtJE36gWjz40XM3U0MZTOqoNkLdGO23rkRkmUNf4");
+	Define("CALL_BACK","".$_SERVER['PHP_SELF']);
+	$access_token = '1255013240205045769-1OnSha5xqvZk7Gf9a34avJfWc7fek4';
+	$access_token_secret = 'YoLqNGWWjCoz5olOfNbSNv3XyP324WqcOCrf6MUWfXp9h';
+?>
